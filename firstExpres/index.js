0ff9aeb9336ca8e65a53a814e990fe8ba44a6186
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2023-09-23 01:14:38 +0100

    first commit

---
+++ firstExpres/index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+//full path for production next 3 rows
+import{dirname} from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res) =>{
+console.log(req);
+res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/info",(req,res) =>{
+    console.log(req);
+    res.send("Just test info !");
+    });
+    
+app.get("/user/:userId",(req,res) =>{
+    const user = req.params.userId;
+    res.send(`Result of GET - User ID ${user}`);
+    });
+    
+
+app.get("/costomer/:customerId",(req,res) =>{
+    const user = req.params.customerId;
+    res.send({customerID : user, name: "custom" + user,respons:'JSON file'});
+    });
+ 
+app.get("/download-png",(req,res) => {
+    res.download(__dirname + "/images/picture1.png")
+    });
+
+app.get("/file-browser",(req,res)=>{
+    res.sendFile(__dirname + "/images/picture1.png")
+})
+
+app.get("/file-pdf",(req,res)=>{
+    res.sendFile(__dirname + "/images/justpdf.pdf")
+})
+
+
+app.get("*",(req,res) =>{
+   console.log(req);
+   res.status(404);
+   res.sendFile(__dirname + "/public/error.html")
+   });
+
+
+
+
+app.listen(port,()=>{
+console.log(`Express started on port ${port}`);
+});
